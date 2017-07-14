0b54e37e2cc9daeac833c36f6b7523ac4ac57be8
Tahir Naushasd <t.naushad25@gmail.com>
2017-07-14 23:05:31 +0100

    added events

--- Fiver.Security.Bearer/Startup.cs
+++ Fiver.Security.Bearer/Startup.cs
@@ -1,9 +1,12 @@
 ﻿using Fiver.Security.Bearer.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Threading.Tasks;
 
 namespace Fiver.Security.Bearer
 {
@@ -32,7 +35,21 @@ namespace Fiver.Security.Bearer
             {
                 AutomaticAuthenticate = true,
                 AutomaticChallenge = true,
-                
+
+                Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                        return Task.CompletedTask;
+                    },
+                    OnTokenValidated = context =>
+                    {
+                        Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                        return Task.CompletedTask;
+                    }
+                },
+
                 TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuer = true,
